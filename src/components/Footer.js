36b7a6784e71c0e293269bbb6a562570fdde2c64
Mostafijur Rahman <mostafijur.rahman299@gmail.com>
2023-06-03 22:54:35 +0600

    Footer component completed

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Layout from "./Layout";
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <footer className="w-full border-t-2 border-solid border-primaryDark font-medium text-lg">
+      <Layout className="py-8 flex items-center justify-between">
+        <span>{new Date().getFullYear()} &copy; All Rights Reversed.</span>
+        <div className="flex items-center">
+          Build with <span className="text-primary text-2xl px-1">&#9825;</span>
+          <Link href="/" className="underline underline-offset-2">DepNox</Link>
+        </div>
+        <Link href="/" className="underline underline-offset-2 ">Say Hello</Link>
+      </Layout>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,8 @@ import ProfilePicture from "../../public/images/profile/Mostafijur-Rahman.png";
 import AnimatedText from "@/components/AnimatedText";
 import Link from "next/link";
 import { LinkArrow } from "@/components/Icons";
+import HireMe from "@/components/HireMe";
+import LightBulb from '../../public/images/svgs/miscellaneous_icons_1.svg'
 
 export default function Home() {
   return (
@@ -35,15 +37,20 @@ export default function Home() {
                 web development.
               </p>
               <div className="flex items-center self-start mt-2">
-                <Link href="/dummy.pdf" target={"_blank"} className="flex items-center bg-dark text-light p-2.5 
+                <Link
+                  href="/dummy.pdf"
+                  target={"_blank"}
+                  className="flex items-center bg-dark text-light p-2.5 
                 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2
-                border-solid border-transparent hover:border-dark" download={true}>
-                  Resume <LinkArrow className={"w-6 ml-1"}/>
+                border-solid border-transparent hover:border-dark"
+                  download={true}
+                >
+                  Resume <LinkArrow className={"w-6 ml-1"} />
                 </Link>
                 <Link
                   href="mailto:mostafijur.rahman299@gmail.com"
                   target={"_blank"}
-                  className="ml-4 text-large font-medium capitalize text-dark underline "
+                  className="ml-4 text-lg font-medium capitalize text-dark underline "
                 >
                   Contact
                 </Link>
@@ -51,6 +58,13 @@ export default function Home() {
             </div>
           </div>
         </Layout>
+
+        <HireMe />
+
+        <div className="absolute right-8 bottom-8 inline-block w-24">
+          <Image src={LightBulb} alt="Mostafijur" className="w-full h-auto" />
+        </div>
+
       </main>
     </>
   );
